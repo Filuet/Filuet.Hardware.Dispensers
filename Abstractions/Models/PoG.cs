3fae31428545ad99dec5e59aeaa41d066ed4f898
Igor Gutsaev <igor.gutsaev@filuet.ru>
2024-01-10 16:00:06 +0300

    Ignore inactive routes while calculating remains

--- Abstractions/Models/PoG.cs
+++ Abstractions/Models/PoG.cs
@@ -130,10 +130,10 @@ namespace Filuet.Hardware.Dispensers.Abstractions.Models
         public IEnumerable<string> Addresses => Routes.Select(x => x.Address);
 
         [JsonIgnore]
-        public int Quantity => Routes?.Sum(x => x.Quantity) ?? 0;
+        public int Quantity => Routes?.Where(x => x.Active ?? false).Sum(x => x.Quantity) ?? 0;
 
         [JsonIgnore]
-        public int MaxQuantity => Routes?.Sum(x => x.MaxQuantity) ?? 0;
+        public int MaxQuantity => Routes?.Where(x => x.Active ?? false).Sum(x => x.MaxQuantity) ?? 0;
 
         public override string ToString() => ProductUid;
     }
