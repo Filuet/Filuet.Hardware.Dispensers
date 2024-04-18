58b39a097c6d1c71429256659adef2dfe8aa1408
Igor Gutsaev <igor.gutsaev@filuet.com>
2024-04-18 16:38:54 +0300

    +cors

--- ExpoExtractor/Program.cs
+++ ExpoExtractor/Program.cs
@@ -10,8 +10,17 @@ using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+builder.Services.AddCors(options => {
+    options.AddPolicy("MyAllowedOrigins",
+        policy => {
+            policy.WithOrigins("http://localhost:5100") // note the port is included 
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
+// Add services to the container
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
