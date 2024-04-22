734baae32c13c0ccea4d02455f2700a364ebf47d
Igor Gutsaev <igor.gutsaev@filuet.com>
2024-04-22 10:24:39 +0300

    statuc activity fix

--- ExpoExtractor/Controllers/DispensingController.cs
+++ ExpoExtractor/Controllers/DispensingController.cs
@@ -33,7 +33,8 @@ namespace ExpoExtractor.Controllers
 
         [HttpGet("status")]
         public IActionResult Status() {
-            Console.WriteLine($"{DateTime.Now:HH:mm:ss}: Status requested. Current status is {StatusSingleton.Status.Action}");
+            if (StatusSingleton.Status != null)
+                Console.WriteLine($"{DateTime.Now:HH:mm:ss}: Status requested. Current status is {StatusSingleton.Status.Action}");
 
             if (StatusSingleton.Status == null || string.IsNullOrWhiteSpace(StatusSingleton.Status.Status))
                 StatusSingleton.Status = new CurrentStatus { Action = "pending", Status = "success", Message = "Waiting for command" };
