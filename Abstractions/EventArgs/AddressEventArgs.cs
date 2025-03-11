186bcce89c40dee6de0685e03de1a220db933907
Shashank Sood <shashanksud@gmail.com>
2025-03-11 12:28:54 +0530

    feat: Add sku names in status message for dispensing and dispensed event methods
    - It will help us get sku-address mapping in ui

--- Abstractions/EventArgs/AddressEventArgs.cs
+++ Abstractions/EventArgs/AddressEventArgs.cs
@@ -8,11 +8,11 @@
         public string address { get; set; }
         public string message { get; set; }
 
-        public static AddressEventArgs DispensingStarted(string address, string sessionId)
-            => new AddressEventArgs { address = address, sessionId = sessionId , message = "dispensing started" };        
+        public static AddressEventArgs DispensingStarted(string address, string sessionId, string skuName)
+            => new AddressEventArgs { address = address, sessionId = sessionId , message = $"{address} dispensing started for sku:{skuName.Trim()}" };        
         
-        public static AddressEventArgs DispensingFinished(string address, string sessionId)
-            => new AddressEventArgs { address = address, sessionId = sessionId , message = "dispensing finished" };
+        public static AddressEventArgs DispensingFinished(string address, string sessionId, string skuName)
+            => new AddressEventArgs { address = address, sessionId = sessionId, message = $"{address} dispensing finished for sku:{skuName.Trim()}" };
 
         public override string ToString() => $"[{address}] {GetType().Name} {message}";
     }
