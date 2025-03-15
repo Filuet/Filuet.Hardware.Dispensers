7356ff584a7b5ad518bf9884150849892b729150
Shashank Sood <shashanksud@gmail.com>
2025-03-15 16:03:43 +0530

    refactor: Enqueue status in DispensingController and enhance status management
    - Improved status management with thread-safe ConcurrentQueue in StatusSingleton so that status are not overwritten by any other event invocation
    - Added api methods for retrieving and resetting statuses.
    - Clear previous status before starting a new dispense request

--- ExpoExtractor/CurrentStatus.cs
+++ ExpoExtractor/CurrentStatus.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace Filuet.Hardware.Dispenser
 {
@@ -16,6 +18,35 @@ namespace Filuet.Hardware.Dispenser
 
     public class StatusSingleton
     {
-        public static CurrentStatus Status { get; set; }
+        private static readonly ConcurrentQueue<CurrentStatus> _statusQueue = new();
+
+        public static void AddStatus(CurrentStatus status)
+        {
+
+            if (status != null && !string.IsNullOrWhiteSpace(status.Status))
+            {
+                _statusQueue.Enqueue(status);
+            }
+        }
+
+        public static CurrentStatus GetLatestStatus()
+        {
+            if (_statusQueue.TryDequeue(out var status) && !string.IsNullOrWhiteSpace(status?.Status))
+            {
+                return status;
+            }
+            return new CurrentStatus { Action = "pending", Status = "success", Message = "Waiting for command" };
+        }
+
+        public static IEnumerable<CurrentStatus> GetAllStatuses()
+        {
+            return _statusQueue.ToArray();
+        }
+
+        public static void ClearStatuses()
+        {
+            while (_statusQueue.TryDequeue(out _)) { }
+        }
     }
+
 }
